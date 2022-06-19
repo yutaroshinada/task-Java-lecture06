de59bb100046a40ce34d5688a20375a30c24b7e1
yutaroshinada <d0.0bd6.6bd9.9b@gmail.com>
2022-06-19 22:59:10 +0900

    第6回課題　現在の日時を取得

--- HelloWorldController.java
+++ HelloWorldController.java
@@ -3,11 +3,27 @@ package com.raisetech.helloworld;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
 @RestController
 public class HelloWorldController {
 
-    @GetMapping("/hello")
-    public String hello() {
+    @GetMapping("/HelloWorld")
+    public String HelloWorld() {
         return "hello world";
+    }
+
+
+    @GetMapping("/date")
+    public static String main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(now);
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        System.out.println(now.format(formatter));
+
+        return (now.format(formatter));
     }
 }
