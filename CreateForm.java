c9809a54f8fc6dbbceab133860ede1d86441d18f
yutaroshinada <d0.0bd6.6bd9.9b@gmail.com>
2022-07-03 14:02:05 +0900

    第７回課題

---
+++ CreateForm.java
@@ -0,0 +1,14 @@
+package com.raisetech.helloworld;
+
+public class CreateForm {
+
+    private String menu;
+
+    public String getMenu() {
+        return menu;
+    }
+
+    public void setMenu(String menu) {
+        this.menu = menu;
+    }
+}
